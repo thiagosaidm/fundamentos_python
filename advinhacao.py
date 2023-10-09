6ece619af621068c8b04cad1dbe5161cf4d4a3f2
thiagosaidm <thiag.said@gmail.com>
2023-10-09 07:13:36 -0300

    "Aula 2"

--- advinhacao.py
+++ advinhacao.py
@@ -1,1 +1,18 @@
+print("********************************")
 print("bem vindo ao jogo de adivinhação")
+print("********************************")
+
+numero_secreto = 42
+
+entrada = input("Digite um número: ")
+numero_usuario = int(entrada)
+
+
+print("você escolheu o número:", numero_usuario)
+
+if (numero_secreto == numero_usuario):
+    print("Você acertou")
+elif (numero_secreto > numero_usuario):
+    print("chute baixo")
+else:
+    print("chute alto")
