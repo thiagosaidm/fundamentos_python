c06dddfc22741724cf8afc7e8918d0f0776a0314
thiagosaidm <thiag.said@gmail.com>
2023-10-17 20:08:39 -0300

    "Aula 5 - Níveis"

--- advinhacao.py
+++ advinhacao.py
@@ -1,36 +1,44 @@
 import random
 
 print("********************************")
-print("BEM VINDE AO JOGO DE ADVINHAÇÃO")
+print("BEM VINDO AO JOGO DE ADVINHAÇÃO")
 print("********************************")
 
+
 while True:
+    total_tentativas = 0
+
     iniciar_jogo = input("****PRESSIONE ->1<- PARA INICIAR****   ")
 
     if (iniciar_jogo == '1'):
         print("****VAMOS COMEÇAR O JOGO****")
+
+        print("Selecione a dificuldade")
+
+        dificuldade = int(input("Fácil (1) // Médio(2) // Dífícil (3):"))
+
+        if(dificuldade == 1):
+            total_tentativas = 10
+        elif(dificuldade == 2):
+            total_tentativas = 5
+        else:
+            total_tentativas = 3
+
         print("DICA: ESCOLHA UM NÚMERO DE 1 A 10")
-        print("VOCÊ TEM 3 CHANCES")
         break
     else:
         print("DIGITE ->1<- PARA COMEÇAR")
 
 numero_secreto = random.randint(1, 10)
 
-total_tentativas = 3
 tentativa = 1
 
 
-
-
 while (tentativa <= total_tentativas):
-    entrada = input("Digite um número: ")
-    numero_usuario = int(entrada)
-
-
+    numero_usuario = int(input("Digite um número: "))
 
     print("tentativa {} de {}".format(tentativa, total_tentativas))
-    print("você escolheu o número:{}".format(numero_usuario))
+
 
     if (numero_usuario < 1 or numero_usuario > 10):
         print("O número deve estar entre 1 e 10")
@@ -44,13 +52,12 @@ while (tentativa <= total_tentativas):
     else:
         print("CHUTE ALTO")
 
-    if (tentativa == 2):
-        print("Ultima chance, escolha com sabedoria")
-    elif (tentativa == 3):
+    tentativa = tentativa + 1
+
+    if (total_tentativas < tentativa):
+        print("você escolheu o número:{}".format(numero_usuario))
         print("O NUMERO ERA:{}".format(numero_secreto))
         print("******GAME OVER******")
-
-    tentativa = tentativa + 1
 
 while True:
     escolha_input = input("***** APERTE -> 0 PARA SAIR || APERTE ->1 PARA TENTAR NOVAMENTE ")
@@ -62,32 +69,40 @@ while True:
         print("********************************")
         print("bem vindo ao jogo de adivinhação")
         print("********************************")
+        print("Selecione a dificuldade")
+
+        dificuldade = int(input("Fácil (1) // Médio(2) // Dífícil (3):"))
+        total_tentativas = 0
+
+        if (dificuldade == 1):
+            total_tentativas = 10
+        elif (dificuldade == 2):
+            total_tentativas = 5
+        else:
+            total_tentativas = 3
 
         numero_secreto = random.randint(1, 10)
 
-        total_tentativas = 3
         tentativa = 1
 
         while (tentativa <= total_tentativas):
-            entrada = input("Digite um número: ")
-            numero_usuario = int(entrada)
+            numero_usuario = int(input("Digite um número: "))
             print("tentativa {} de {}".format(tentativa, total_tentativas))
-            print("você escolheu o número:{}".format(numero_usuario))
 
             if (numero_secreto == numero_usuario):
                 print("PARABÉNS, VOCÊ ACERTOU O NUMERO")
+                print("O NUMERO ERA:{}".format(numero_secreto))
                 break
             elif (numero_secreto > numero_usuario):
                 print("CHUTE BAIXO")
             else:
                 print("CHUTE ALTO")
 
-            if (tentativa == 2):
-                print("Ultima chance, escolha com sabedoria")
-            elif (tentativa == 3):
+            tentativa = tentativa + 1
+
+            if (total_tentativas < tentativa):
+                print("você escolheu o número:{}".format(numero_usuario))
                 print("O NUMERO ERA:{}".format(numero_secreto))
                 print("******GAME OVER******")
-
-            tentativa = tentativa + 1
     else:
         print("ESCOLHA UMA OPÇÃO VÁLIDA(0 OU 1)")
---
+++ excercicios.py
@@ -0,0 +1,6 @@
+dia_inicial = 20
+dia_final = 28
+mes = "fevereiro"
+ano = 2024
+
+print("EM {} O CARNAVAL ACONTECE EM {} DO DIA {} ATÉ {}". format(ano, mes, dia_inicial, dia_final))
