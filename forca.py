9161cf4a4caec6f9eb7abfae510b06b2d38af8a8
thiagosaidm <thiag.said@gmail.com>
2023-10-21 11:06:30 -0300

    "Aula 9 - Avançando no Python: Jogo da Forca - Manipulação de Strings, Bools"

--- forca.py
+++ forca.py
@@ -1,12 +1,13 @@
 import random
-def jogar():
 
+
+def jogar():
     print("**********************************************")
     print("**********BEM VINDO AO JOGO DA FORCA**********")
     print("**********************************************")
 
-    #define-se as variáveis de regra do jogo: Enforcou = Perdeu | Acertou = Ganhou,
-    #Enquanto não acertar e/ou ganhar: continuar a jogar
+    # define-se as variáveis de regra do jogo: Enforcou = Perdeu | Acertou = Ganhou,
+    # Enquanto não acertar e/ou ganhar: continuar a jogar
     # Abaixo teremos a palavra secreta
 
     # O programa tem q aceitar letra minuscula e maiscula e apenas letras:
@@ -15,29 +16,29 @@ def jogar():
 
     palavra_secreta = "Python"
     palavra_secreta = palavra_secreta.lower()
-    #A função len() me diz o tamanho da string
+    # A função len() me diz o tamanho da string
     quantidade_letras = len(palavra_secreta)
+    letras_acertadas = ["_"] * quantidade_letras
 
     acertou = False
     enforcou = False
 
-    while(not acertou and not enforcou):
+    while (not acertou and not enforcou):
         print("Jogando")
         print("A palavra têm {} letras".format(quantidade_letras))
+        print(" ".join(letras_acertadas))
         chute = input("Digite uma letra:")
-        if(chute.isalpha()):
+        if (chute.isalpha()):
             chute = chute.lower()
         else:
             print("Você deve digitar apenas letras")
 
         index = 0
-
         for letra in palavra_secreta:
-            index = index + 1
             if(chute == letra):
-                print("Têm a {} na posição {}". format(letra, index))
-
-
+                letras_acertadas[index] = letra
+            index = index + 1
+        print(letras_acertadas)
 
 if (__name__ == "__main__"):
     jogar()
