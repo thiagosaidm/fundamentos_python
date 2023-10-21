d87a5f34f2e0336be5fea235d63767c0e6ebce3d
thiagosaidm <thiag.said@gmail.com>
2023-10-21 10:26:23 -0300

    "Aula 9 - Avançando no Python: Jogo da Forca - Manipulação de Strings, Bools"

--- forca.py
+++ forca.py
@@ -5,11 +5,38 @@ def jogar():
     print("**********BEM VINDO AO JOGO DA FORCA**********")
     print("**********************************************")
 
-    pontuacao_total = 0
+    #define-se as variáveis de regra do jogo: Enforcou = Perdeu | Acertou = Ganhou,
+    #Enquanto não acertar e/ou ganhar: continuar a jogar
+    # Abaixo teremos a palavra secreta
 
+    # O programa tem q aceitar letra minuscula e maiscula e apenas letras:
+    #  - Para isso é preciso tratar a entrada, usei a função lower()
+    #  - Para saber se é uma letra, usei isaplha()
 
-    print("Você fez {} pontos no total".format(pontuacao_total))
-    print("******GAME OVER******")
+    palavra_secreta = "Python"
+    palavra_secreta = palavra_secreta.lower()
+    #A função len() me diz o tamanho da string
+    quantidade_letras = len(palavra_secreta)
+
+    acertou = False
+    enforcou = False
+
+    while(not acertou and not enforcou):
+        print("Jogando")
+        print("A palavra têm {} letras".format(quantidade_letras))
+        chute = input("Digite uma letra:")
+        if(chute.isalpha()):
+            chute = chute.lower()
+        else:
+            print("Você deve digitar apenas letras")
+
+        index = 0
+
+        for letra in palavra_secreta:
+            index = index + 1
+            if(chute == letra):
+                print("Têm a {} na posição {}". format(letra, index))
+
 
 
 if (__name__ == "__main__"):
