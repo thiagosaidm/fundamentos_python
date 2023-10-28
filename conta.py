4dfebb8b8f535f1361e5b5faa24ff3f8b37f5d87
thiagosaidm <thiag.said@gmail.com>
2023-10-28 10:50:38 -0300

    "Aula 03 - Métodos e atributos"

--- conta.py
+++ conta.py
@@ -1,11 +1,25 @@
 
 #Classes e Objetos
 
-
 class Conta:
     def __init__(self, num, titular, saldo, limite): #init é o mesmo que o constructor
         self.num = num # usa-se self para fazer referencia a atributos e metodos da classe
-        self.titutlar = titular
+        self.titular = titular
         self.saldo = saldo
         self.limite = limite
         print("Conta Criada {}". format(titular))
+    def extrato(self):
+            print("O saldo do titular {}, é de {}". format(self.titular, self.saldo))
+
+    def depositar(self):
+        valor_deposito = int(input("Digite o valor a ser depositado"))
+        print("O valor de R${} foi depositado em conta". format(valor_deposito))
+        self.saldo += valor_deposito
+
+    def sacar(self):
+        valor_saque = int(input("Digite o valor a ser sacado"))
+        if(valor_saque <= self.saldo):
+            self.saldo -= valor_saque
+            print("Você sacou R${} da sua conta". format(valor_saque))
+        else:
+            print("Você não tem saldo suficiente para essa operação, seu saldo é de R${}". format(self.saldo))
